6bc24c6da439c0659c353b6ce7bf99a4431cf743
Mahesh <91116963+MaheshKathir@users.noreply.github.com>
2023-07-11 17:53:20 +0530

    Update balanceCont(kpmg).py

--- balanceCont(kpmg).py
+++ balanceCont(kpmg).py
@@ -1,3 +1,5 @@
+# Pypdf FileReader and Writer  Deprecated Error -> pip install 'PyPDF2<3.0'
+
 import pdfplumber
 import re
 import fitz
@@ -24,7 +26,9 @@ if password == list:
             
             start_investment = 'Investments'
             end_investment = "Other Assets"
+            
             investment_summary = 'Investment Summary'
+            
 
             matched_values = {}
             matched_client_values = {}
@@ -186,6 +190,7 @@ if password == list:
                             data_lines = []
                             for subsequent_line in lines[lines.index(line) + 1:]:
                                 values = re.findall(r'\b\d{1,3}(?:,\d{3})*(?:\.\d+)?\b', subsequent_line)
+                                print('client_name:', subsequent_line)
                                 if values:
                                     data_lines.append(values[0]) 
                                 if end_account in subsequent_line:
@@ -228,7 +233,39 @@ if password == list:
                         client_nameTo_placeLink = client_name_page
                         
                         client_data_no = line_data
-                    
+                        consolidated_memberString= 'Consolidated Member Benefit Totals'
+                        matched_consolidatedPageNo = None
+
+                        for page_num, page in enumerate(pdf.pages):
+                            lines = page.extract_text().split('\n')
+                            for line in lines:
+                                 if consolidated_memberString in line:
+                                     for consolidated_subsequent_line in lines[lines.index(line) + 1:]:
+                                         if line_data in consolidated_subsequent_line:
+                                             matched_consolidatedPageNo = page_num
+                                             break
+                            if matched_consolidatedPageNo is None:
+                                i = page_num + 1
+                                for date in enumerate(pdf.pages):
+                                    try:
+                                        next_line = pdf.pages[i].extract_text().split('\n')
+                                        for end_line in next_line:
+                                            if consolidated_memberString in end_line:
+                                                for subsequent_line in next_line[next_line.index(end_line) + 1:]:
+                                                    if line_data in subsequent_line:
+                                                        matched_consolidatedPageNo = i
+                                                        print('consolidated_value:', matched_consolidatedPageNo)
+                                                        break
+                                        if matched_consolidatedPageNo is None:
+                                            i += 1
+                                        else:
+                                            break
+                                    except:
+                                        pass
+                                    
+                                    break
+                                
+                        
                         word_intances = matched_client_page.search_for(client_data_no)
                         if len(word_intances) > 0:
                             for instance in word_intances:
@@ -241,7 +278,7 @@ if password == list:
                                     
                                     pdf_writer.addLink(
                                         client_nameTo_placeLink,
-                                        26,
+                                        matched_consolidatedPageNo,
                                         RectangleObject([x-10, new_y, x1+10, (new_y + (y1 - y))]),
                                         border=[1,1,1]
                                     )
@@ -260,10 +297,45 @@ if password == list:
                         doc = fitz.open(pdf_path)
                         matched_investment_page = doc[investment_page]
                         print("******", matched_investment_page)
-                        investment_subline_palceLink = investment_page
+                        investment_subline_placeLink = investment_page
                         
                         investment_data_no = line_data
                         
+                        #To find the investment summary page number
+                        investment_summary = 'Investment Summary'
+                        matched_investment_pageNo = None
+                        
+                        for page_num, page in enumerate(pdf.pages):
+                            lines = page.extract_text().split('\n')
+                            for line in lines:
+                                if investment_summary in line:
+                                    for subsequent_line in lines[lines.index(line) + 1:]:
+                                        if line_data in subsequent_line:
+                                            matched_investment_pageNo = page_num
+                                            break
+                            if matched_investment_pageNo is None:
+                                i = page_num + 1
+                                for date in enumerate(pdf.pages):
+                                    try:
+                                        next_line = pdf.pages[i].extract_text().split('\n')
+                                        for end_line in next_line:
+                                            if investment_summary in end_line:
+                                                for investment_subsequent_line in next_line[next_line.index(end_line) + 1:]:
+                                                    if line_data in investment_subsequent_line:
+                                                        matched_investment_pageNo = i
+                                                        print('investment_value:', matched_investment_pageNo)
+                                                        break
+                                        if matched_investment_pageNo is None:
+                                            i += 1
+                                        else:
+                                            break
+                                    except:
+                                       pass
+                                    # print(matched_goto_pagenumber)
+                                    # print(to_find)
+                                    break
+                                        
+                                                                    
                         word_instances = matched_investment_page.search_for(investment_data_no)
                         if len(word_instances) > 0:
                             for instance in word_instances:
@@ -274,8 +346,8 @@ if password == list:
                                     
                                     new_y = page_height - y1
                                     pdf_writer.addLink(
-                                        investment_subline_palceLink,
-                                        35,
+                                        investment_subline_placeLink,
+                                        matched_investment_pageNo,
                                         RectangleObject([x-10, new_y, x1+10, (new_y + (y1 - y))]),
                                         border=[1,1,1]    
                                     )
